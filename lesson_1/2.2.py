b6862305073893f2f365dc52a69817ad2ff1e173
Pimenov.VlaD <Pimenov.vlad@gmail.com>
2026-08-18 14:17:09 +0300

    Lesson_2

---
+++ lesson_1/2.2.py
@@ -0,0 +1,20 @@
+first_name = "Иван"
+last_name = "Петров"
+age = 25
+
+full_name = first_name + " " + last_name
+print(full_name)
+print("Возраст: " + str(age))
+print(len(full_name))
+print("Иван" in full_name)
+print(ord("A"))
+
+
+word = "ха "
+empty = ""
+space = " "
+
+print(word * 3)
+print(len(empty))
+print(len(space))
+#123
---
+++ lesson_1/homework_2.py
@@ -0,0 +1,8 @@
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+
+print(float(count))
+#213
